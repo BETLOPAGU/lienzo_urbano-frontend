80410779101dba9c3126093469747554b8febed3
Nadia <nbla.escom@gmail.com>
2022-12-13 22:54:39 -0600

    Redireccion de pagina con variable de autenticacion - falsa

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,16 +1,43 @@
-import { Route, Routes } from 'react-router-dom';
+import { useEffect } from 'react';
+import { Navigate, Route, Routes } from 'react-router-dom';
+
+import { useQuery, gql } from '@apollo/client';
+
 import { AuthRoutes } from '../auth/routes/AuthRoutes';
 import { LURoutes } from '../LienzoUrbano/routes/LURoutes';
+import { useAuthStore } from '../hooks';
 
 
 export const AppRouter = () => {
-  return (
-    <>
-        <Routes>
-            <Route path="/auth/*" element={ <AuthRoutes /> } />
 
-            <Route path="/*" element={ <LURoutes /> } />
-        </Routes>
-    </>
+  // const { status, checkAuthToken } = useAuthStore();
+
+  // useEffect(() => {
+  //   checkAuthToken();
+  // }, [])
+
+  const authStatus = 'authenticated';
+
+
+  return (
+
+    <Routes>
+      {
+        (authStatus === 'authenticated')
+          ?(
+            <>
+              <Route path="/auth/*" element={<AuthRoutes />} />
+              <Route path="/*" element={<Navigate to="/auth/welcome" />} />
+            </>
+          )
+          :(
+            <>
+              <Route path="/auth/*" element={<AuthRoutes />} />
+              <Route path="/auth/*" element={<AuthRoutes />} />
+            </>
+          )
+      }
+    </Routes>
+
   )
 }
